12799d011a7a480203a4d6ed0a3247a65dc7f748
Osinovsky <dqdongshi@163.com>
2021-06-03 00:00:59 +0800

    0602

--- hot_100/part-2.py
+++ hot_100/part-2.py
@@ -72,3 +72,32 @@ class Solution:
             if max_i >= i and i + jump > max_i: 
                 max_i = i + jump
         return max_i >= i
+# 14. 最小覆盖子串，给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。
+# 滑动窗口
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.defaultdict(int)
+        tset = set(t)
+        for c in tset:
+            need[c] += 1
+        needCnt = len(t)
+        i = 0
+        res = (0, float('inf'))
+        for j, c in enumerate(s):
+            if need[c] > 0:
+                needCnt -= 1
+            need[c] -= 1
+            if needCnt == 0:       #步骤一：滑动窗口包含了所有T元素
+                while True:      #步骤二：增加i，排除多余元素
+                    c = s[i] 
+                    if need[c] == 0:
+                        break
+                    need[c] += 1
+                    i += 1
+                if j - i < res[1] - res[0]:   #记录结果
+                    res = (i, j)
+                need[s[i]] += 1  #步骤三：i增加一个位置，寻找新的满足条件滑动窗口
+                needCnt += 1
+                i+=1
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]
+
