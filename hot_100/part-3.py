7a7aed3caefce87c85a17a16c9b2c857e8e055f0
Alexei Osinovsky <dqdongshi@163.com>
2021-06-06 17:14:18 +0800

    0606

--- hot_100/part-3.py
+++ hot_100/part-3.py
@@ -22,3 +22,17 @@ class Solution:
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return reduce(lambda x, y: x ^ y, nums)
+# 23. 检测链表是否存在环路
+# 快慢指针（要求常数内存的情况下）
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
