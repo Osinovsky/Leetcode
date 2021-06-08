6e5a2302bd6c399a1ac35d6c1b8dab027aeff38f
Alexei Osinovsky <dqdongshi@163.com>
2021-06-08 21:12:25 +0800

    0608

--- hot_100/part-3.py
+++ hot_100/part-3.py
@@ -57,3 +57,96 @@ class Solution:
                 ans = total - 2 * j
                 break
         return ans
+# 25. 最大正方形
+# dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + xij
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+        maxSide = 0
+        rows, columns = len(matrix), len(matrix[0])
+        dp = [[0] * columns for _ in range(rows)]
+        for i in range(rows):
+            for j in range(columns):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    maxSide = max(maxSide, dp[i][j])        
+        maxSquare = maxSide * maxSide
+        return maxSquare
+# 26. 滑动窗口最大值
+# 优先队列, O(nlogn)
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        # 注意 Python 默认的优先队列是小根堆
+        q = [(-nums[i], i) for i in range(k)]
+        heapq.heapify(q)
+        ans = [-q[0][0]]
+        for i in range(k, n):
+            heapq.heappush(q, (-nums[i], i))
+            while q[0][1] <= i - k:
+                heapq.heappop(q)
+            ans.append(-q[0][0])
+        return ans
+# 单调队列, O(n)，如果左边小于右边的值，滑动时左边的值完全可以pop，因为只要右边在它就没用
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        q = collections.deque()
+        for i in range(k):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+        ans = [nums[q[0]]]
+        for i in range(k, n):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            while q[0] <= i - k:
+                q.popleft()
+            ans.append(nums[q[0]])
+        return ans
+# 分块 + 预处理，O(n)
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        prefixMax, suffixMax = [0] * n, [0] * n
+        for i in range(n):
+            if i % k == 0:
+                prefixMax[i] = nums[i]
+            else:
+                prefixMax[i] = max(prefixMax[i - 1], nums[i])
+        for i in range(n - 1, -1, -1):
+            if i == n - 1 or (i + 1) % k == 0:
+                suffixMax[i] = nums[i]
+            else:
+                suffixMax[i] = max(suffixMax[i + 1], nums[i])
+        ans = [max(suffixMax[i], prefixMax[i + k - 1]) for i in range(n - k + 1)]
+        return ans
+# 27. 完全平方数，一个数最少能表示为几个平方数之和
+# 数学算法，每个自然数都可以由最多四个平方数之和表示
+class Solution:
+    def isSquare(self, n: int) -> bool:
+        sq = int(math.sqrt(n))
+        return sq*sq == n
+
+    def numSquares(self, n: int) -> int:
+        # four-square and three-square theorems
+        while (n & 3) == 0:
+            n >>= 2      # reducing the 4^k factor from number
+        if (n & 7) == 7: # mod 8
+            return 4
+
+        if self.isSquare(n):
+            return 1
+        # check if the number can be decomposed into sum of two squares
+        for i in range(1, int(n**(0.5)) + 1):
+            if self.isSquare(n - i*i):
+                return 2
+        # bottom case from the three-square theorem
+        return 3
+
