d9783a87b81652c921f4db7435f41525cb70adef
Alexei Osinovsky <dqdongshi@163.com>
2021-06-11 13:51:35 +0800

    0611

--- hot_100/part-4.py
+++ hot_100/part-4.py
@@ -36,3 +36,44 @@ class Solution:
         self.coins = coins
         if amount < 1: return 0
         return dp(amount)
+# 43. 比特位计数，给定非负整数，记录所有比它小或等于它的非负整数的二进制形式中有多少1
+# Brian Kernighan 算法，直接计算，O(nlogn)
+# x = x & x - 1 将二进制最后一个1变成0
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        def countOnes(x: int) -> int:
+            ones = 0
+            while x > 0:
+                x &= (x - 1)
+                ones += 1
+            return ones
+        bits = [countOnes(i) for i in range(n + 1)]
+        return bits
+# DP 最高有效位，可以根据比i小的数进行DP, O(n)
+# 对 x 如果知道最大的 y 使得 y \leq x 且 y 是 2 的整数次幂，则称 y 是 x 的最高有效位
+# z = x - y 则有 dp[x] = dp[z] + 1。而判断2的幂，当且仅当 y & y - 1 == 0
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits = [0]
+        highBit = 0
+        for i in range(1, n + 1):
+            if i & (i - 1) == 0:
+                highBit = i
+            bits.append(bits[i - highBit] + 1)
+        return bits
+# DP 最低有效位, O(n)
+# 偶数 x, d[x] = d[x//2]，奇数 x, d[x] = d[x//2] + 1
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits = [0]
+        for i in range(1, n + 1):
+            bits.append(bits[i >> 1] + (i & 1))
+        return bits
+# DP 最低设置位，O(n)
+# y = x & x - 1，d[x] = d[y] + 1
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits = [0]
+        for i in range(1, n + 1):
+            bits.append(bits[i & (i - 1)] + 1)
+        return bits
